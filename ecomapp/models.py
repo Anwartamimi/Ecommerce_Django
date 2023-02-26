f8f9daaf7c7be8635ef2695dc53434ebf481d798
AnwarT <anwartamimi90@gmail.comgit config --global user.email anwartamimi90@gmail.com>
2023-02-26 16:19:42 +0200

    project created

---
+++ ecomapp/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=191)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='media', blank=True, default='default.png')
+
+    def __str__(self):
+        return self.name
+    
+class CartItem(models.Model):
+    cart_id = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}:{}'.format(self.product.name, self.id)
+
+    def total_cost(self):
+        return self.price * self.quantity
+    
+    def update_quantity(self, quantity):
+        self.quantity = self.quantity + quantity
+        self.save()
+    
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=191)
+    email = models.EmailField()
+    postal_code = models.IntegerField()
+    address = models.CharField(max_length=191)
+    date = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{}:{}".format(self.id, self.email)
+
+class LineItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{}:{}".format(self.product.name, self.id)
+
+    def cost(self):
+        return self.price * self.quantity
